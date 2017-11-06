5f936682373fe844f111b46a269f5b5f27461d99
Pedro Medina Contreras <pedro.medina@roket.mx>
2017-11-06 00:08:44 -0700

    Modificaciones en app

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express'),
     mongoose = require('mongoose'),
     bodyParser = require('body-parser');
 
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+
 // var db = mongoose.connect('mongodb://localhost/consume-local');
 
 var db = mongoose.connect('mongodb://pedro:m0r0cha95@ds161584.mlab.com:61584/jobfinder');
@@ -12,8 +15,10 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 placeRouter = require('./Routes/placeRoutes')(Place);
 
@@ -33,3 +38,16 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
-var gulp = require('gulp'),
-  nodemon = require('gulp-nodemon');
-
-  gulp.task('default', function(){
-    nodemon({
-      script: 'app.js',
-      ext: 'js',
-      env: {
-        PORT:3000
-      },
-      ignore: ['./node_modules/**']
-    })
-    .on('restart', function(){
-      console.log('Restarting');
-    });
-  });
+// var gulp = require('gulp'),
+//   nodemon = require('gulp-nodemon');
+//
+//   gulp.task('default', function(){
+//     nodemon({
+//       script: 'app.js',
+//       ext: 'js',
+//       env: {
+//         PORT:3000
+//       },
+//       ignore: ['./node_modules/**']
+//     })
+//     .on('restart', function(){
+//       console.log('Restarting');
+//     });
+//   });
